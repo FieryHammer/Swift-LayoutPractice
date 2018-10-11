b2ece04062d29249acc8bdd8183800dab249d587
Horvath, Mate <Mate.Horvath.@misys.com>
2018-10-11 11:48:29 +0200

    🐣 FEATURE : Player struct + data passing with segues

---
+++ LayoutPractice/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  LayoutPractice
+//
+//  Created by Horvath, Mate on 2018. 10. 10..
+//  Copyright © 2018. Finastra. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
